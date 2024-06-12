87c8d6a110c78697b4ad069161eeb8ae75b49f00
Corey Kosak <kosak@kosak.com>
2024-06-11 22:34:22 -0400

    ok let's get rid of this

--- frontend/shared/protocol/control/crequest.ts
+++ frontend/shared/protocol/control/crequest.ts
@@ -244,8 +244,6 @@ export class CRequest {
 
   static tryParseJson(item: any) {
     const [variant] = assertAndDestructure1(item, i => assertArrayOfLength(i, 2));
-    console.log('hi variant is');
-    console.log(variant);
     const [tag, item1] = assertAndDestructure2(variant, i => assertEnum<crequestInfo.Tag>(i), i => i);
     let payload: crequestInfo.payload_t;
     switch (tag) {
