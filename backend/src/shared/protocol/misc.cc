3699db85ad3849a9246dfee803a9686eb351d084
Corey Kosak <kosak@kosak.com>
2024-09-02 15:32:37 -0400

    oh hell no

--- backend/src/shared/protocol/misc.cc
+++ backend/src/shared/protocol/misc.cc
@@ -45,4 +45,23 @@ std::ostream &operator<<(std::ostream &s, const Estimates &o) {
 }
 
 DEFINE_TYPICAL_JSON(Estimates, front_, back_);
+
+Filter::Filter() = default;
+Filter::Filter(
+    std::optional<std::string> sender, std::optional<std::string> instanceExact,
+    std::optional<std::string> instancePrefix, bool strong, uint64_t expirationSecs) :
+    sender_(std::move(sender)), instanceExact_(std::move(instanceExact)),
+    instancePrefix_(std::move(instancePrefix)), strong_(strong), expirationSecs_(expirationSecs) {}
+Filter::Filter(const Filter &other) = default;
+Filter::Filter(Filter &&other) noexcept = default;
+Filter &Filter::operator=(const Filter &other) = default;
+Filter &Filter::operator=(Filter &&other) noexcept = default;
+Filter::~Filter() = default;
+
+std::ostream &operator<<(std::ostream &s, const Filter &o) {
+  return streamf(s, "Filter(%o,%o,%o,%o,%o)", o.sender_, o.instanceExact_, o.instancePrefix_,
+      o.strong_, o.expirationSecs_);
+}
+
+DEFINE_TYPICAL_JSON(Filter, sender_, instanceExact_, instancePrefix_, strong_, expirationSecs_);
 }  // namespace z2kplus::backend::shared::protocol
